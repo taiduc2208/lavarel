aab85b6e67e1d47449e5e4461b39a82063455a28
Abu <ductrieutaihust2208@gmail.com>
2022-01-24 17:13:26 +0700

    done example laravel

--- laravel-cart-cloudways/routes/web.php
+++ laravel-cart-cloudways/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index'])->name('home');
+
+Route::get('/shop', [ProductController::class, 'index'])->name('shop');
+
+Route::get('/shop/{id}', [ProductController::class, 'show'])->name('product');
+
+Route::get('/cart', [CartController::class, 'cart'])->name('cart');
+Route::get('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/delete-from-cart/{id}', [CartController::class, 'delete'])->name('delete.from.cart');
+Route::post('/update-from-cart/{id}', [CartController::class, 'update'])->name('update.from.cart');
